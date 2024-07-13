a93593676b87a0bc56b6cfadf0d5953588c1fbcf
Rodrigo <rbz991@hotmail.com>
2024-07-13 16:02:25 -0600

    Fixed a bug preventing startup while trying to measure component durations

--- SetUp.vb
+++ SetUp.vb
@@ -136,6 +136,7 @@
                 ReDim AC(i).FeedbackType(1)
                 ReDim AC(i).DelayDuration(1)
                 ReDim AC(i).DelayType(1)
+                ReDim AC(i).ComponentDuration_measured(AC(i).ComponentIteration)
                 AC(i).HouselightOnOff = fileReader.ReadLine().Replace("#", "")
                 AC(i).COD = fileReader.ReadLine()
                 AC(i).MaxRefs = fileReader.ReadLine()
